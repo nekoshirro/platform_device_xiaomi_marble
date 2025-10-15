nekoshirro/platform_device_xiaomi_marble

commit 2c9b6145c533230dd7d5a7a3c62116a26e041a20

sm8450-common: Remove more nfc related features for non nfc skus
As seen on stock marble vs marblein

Change-Id: If4491113e60908017c33d9eaef102a3922334f7c
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

common.mk
@@ -307,12 +307,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
-# NFC
+# NFC / Secure Element
 PRODUCT_PACKAGES += \
     android.hardware.nfc-service.nxp \
     android.hardware.nfc@1.2.vendor \
+    android.hardware.secure_element@1.2.vendor \
     com.android.nfc_extras \
     libchrome.vendor
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml
 
 ifeq ($(TARGET_NFC_SUPPORTED_SKUS),)
 TARGET_COPY_OUT_NFC_SKU_PERMISSIONS := $(TARGET_COPY_OUT_VENDOR)/etc/permissions/
@@ -328,6 +332,7 @@ $(foreach sku_out, $(TARGET_COPY_OUT_NFC_SKU_PERMISSIONS), \
         frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(sku_out)/android.hardware.nfc.hcef.xml \
         frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(sku_out)/android.hardware.nfc.uicc.xml \
         frameworks/native/data/etc/android.hardware.nfc.xml:$(sku_out)/android.hardware.nfc.xml \
+        frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(sku_out)/etc/permissions/android.hardware.se.omapi.ese.xml \
         frameworks/native/data/etc/com.android.nfc_extras.xml:$(sku_out)/com.android.nfc_extras.xml \
         frameworks/native/data/etc/com.nxp.mifare.xml:$(sku_out)/com.nxp.mifare.xml))
 
@@ -399,14 +404,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.3.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
-
-# Secure element
-PRODUCT_PACKAGES += \
-    android.hardware.secure_element@1.2.vendor
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.ese.xml \
-    frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml
 
 # Sensors
 PRODUCT_PACKAGES += \

vintf/manifest_nfc.xml
@@ -8,8 +8,40 @@
         <name>android.hardware.nfc</name>
         <fqname>INfc/default</fqname>
     </hal>
+    <!-- secure element -->
+    <hal format="hidl" override="true">
+       <name>android.hardware.secure_element</name>
+       <transport>hwbinder</transport>
+       <version>1.2</version>
+       <interface>
+           <name>ISecureElement</name>
+           <instance>eSE1</instance>
+           <instance>SIM1</instance>
+           <instance>SIM2</instance>
+       </interface>
+    </hal>
     <hal format="aidl" override="true">
         <name>vendor.nxp.nxpnfc_aidl</name>
         <fqname>INxpNfc/default</fqname>
+    </hal>
+    <!-- eSEPowerManager -->
+    <hal format="hidl" override="true">
+        <name>vendor.qti.esepowermanager</name>
+        <transport>hwbinder</transport>
+        <version>1.1</version>
+        <interface>
+            <name>IEsePowerManager</name>
+            <instance>default</instance>
+        </interface>
+    </hal>
+    <!--  Eid service -->
+    <hal format="hidl" override="true">
+        <name>vendor.qti.hardware.eid</name>
+        <transport>hwbinder</transport>
+        <version>1.0</version>
+        <interface>
+            <name>IEid</name>
+            <instance>default</instance>
+        </interface>
     </hal>
 </manifest>

vintf/manifest_no_nfc.xml
@@ -7,7 +7,28 @@
     <hal format="aidl" override="true">
         <name>android.hardware.nfc</name>
     </hal>
+    <!-- secure element -->
+    <hal format="hidl" override="true">
+       <name>android.hardware.secure_element</name>
+       <transport>hwbinder</transport>
+       <version>1.2</version>
+       <interface>
+           <name>ISecureElement</name>
+           <instance>SIM1</instance>
+           <instance>SIM2</instance>
+       </interface>
+    </hal>
     <hal format="aidl" override="true">
         <name>vendor.nxp.nxpnfc_aidl</name>
+    </hal>
+    <!-- eSEPowerManager -->
+    <hal format="hidl" override="true">
+        <name>vendor.qti.esepowermanager</name>
+        <transport>hwbinder</transport>
+    </hal>
+    <!--  Eid service -->
+    <hal format="hidl" override="true">
+        <name>vendor.qti.hardware.eid</name>
+        <transport>hwbinder</transport>
     </hal>
 </manifest>

commit 6605d40eed7efa912290591a59e71330c74ee988

sm8450-common: Fix copy path for android.hardware.se.omapi.ese.xml
It seems that SystemConfig cannot read permission android.hardware.se.omapi.ese.xml
from /odm/etc/permissions/sku_marble/etc/permissions:
I SystemConfig: Reading permissions from /odm/etc/permissions/sku_marble/com.nxp.mifare.xml
I SystemConfig: Reading permissions from /odm/etc/permissions/sku_marble/com.android.nfc_extras.xml
I SystemConfig: Reading permissions from /odm/etc/permissions/sku_marble/android.hardware.nfc.xml
I SystemConfig: Reading permissions from /odm/etc/permissions/sku_marble/android.hardware.nfc.hce.xml
I SystemConfig: Reading permissions from /odm/etc/permissions/sku_marble/android.hardware.nfc.hcef.xml
I SystemConfig: Reading permissions from /odm/etc/permissions/sku_marble/android.hardware.nfc.uicc.xml
I SystemConfig: Reading permissions from /odm/etc/permissions/sku_marble/android.hardware.nfc.ese.xml
...

After android.hardware.se.omapi.ese.xml is moved to /odm/etc/permissions/sku_marble:
I SystemConfig: Reading permissions from /odm/etc/permissions/sku_marble/android.hardware.nfc.xml
I SystemConfig: Reading permissions from /odm/etc/permissions/sku_marble/android.hardware.se.omapi.ese.xml <== Read successfully
I SystemConfig: Reading permissions from /odm/etc/permissions/sku_marble/android.hardware.nfc.hcef.xml
I SystemConfig: Reading permissions from /odm/etc/permissions/sku_marble/android.hardware.nfc.ese.xml
I SystemConfig: Reading permissions from /odm/etc/permissions/sku_marble/android.hardware.nfc.uicc.xml
I SystemConfig: Reading permissions from /odm/etc/permissions/sku_marble/com.nxp.mifare.xml
I SystemConfig: Reading permissions from /odm/etc/permissions/sku_marble/com.android.nfc_extras.xml
I SystemConfig: Reading permissions from /odm/etc/permissions/sku_marble/android.hardware.nfc.hce.xml
...

Fixes: 2c9b614 ("sm8450-common: Remove more nfc related features for non nfc skus")
Change-Id: I6e82772c923b46a29796951922a436596763dce5

common.mk
@@ -268,7 +268,7 @@ $(foreach sku_out, $(TARGET_COPY_OUT_NFC_SKU_PERMISSIONS), \
         frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(sku_out)/android.hardware.nfc.hcef.xml \
         frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(sku_out)/android.hardware.nfc.uicc.xml \
         frameworks/native/data/etc/android.hardware.nfc.xml:$(sku_out)/android.hardware.nfc.xml \
-        frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(sku_out)/etc/permissions/android.hardware.se.omapi.ese.xml \
+        frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(sku_out)/android.hardware.se.omapi.ese.xml \
         frameworks/native/data/etc/com.android.nfc_extras.xml:$(sku_out)/com.android.nfc_extras.xml \
         frameworks/native/data/etc/com.nxp.mifare.xml:$(sku_out)/com.nxp.mifare.xml))
 