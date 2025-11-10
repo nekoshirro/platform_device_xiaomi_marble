864181c84e431d090ba02c7d53964beeee1be849
Fiqri Ardyansyah <fiqri191002@gmail.com>
2025-11-10 09:55:23 +0000

    marble: Init PixelOS
    Change-Id: I3583f0d8ff3220a9b2b47b0dd9333f52cbbe0d30
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- device.mk
+++ device.mk
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+-include vendor/private/keys/keys.mk
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 TARGET_SUPPORTS_OMX_SERVICE := false
@@ -317,7 +319,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     DialerResXiaomi \
     FrameworksResXiaomi \
-    LineageResXiaomi \
     SettingsProviderResXiaomi \
     SettingsResXiaomi
 
