19fd900cba8357d827476560a0172009ccc8ff98
Hafidz Muzakky <ais.muzakky@gmail.com>
2026-04-23 15:21:50 +0700

    marble: Inherit TouchServices package
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,3 @@
 git clone https://github.com/nekoshirro/android_hardware_dolby.git -b aospa hardware/dolby
 git clone https://github.com/kenway214/packages_apps_GameBar.git packages/apps/GameBar
+git clone https://github.com/kenway214/packages_apps_TouchServices.git packages/apps/TouchServices
