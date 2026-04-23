b1ffd49e755104d14682e8c2dcfc7bdf9d7f1690
Hafidz Muzakky <ais.muzakky@gmail.com>
2026-04-23 15:21:50 +0700

    marble: Inherit TouchServices package
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -199,6 +199,7 @@ VENDOR_SECURITY_PATCH := 2026-02-01
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
+SELINUX_IGNORE_NEVERALLOWS := true
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
