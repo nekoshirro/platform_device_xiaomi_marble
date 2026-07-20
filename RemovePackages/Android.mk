75464a4737ea3b37cdd9c96dbbe2a99975720a0d
Hafidz Muzakky <ais.muzakky@gmail.com>
2026-07-21 06:27:59 +0700

    marble: removepackages: Build updater now
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -5,9 +5,8 @@ LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
-    Updater \
-    GameSpace \
-    bcr
+    bcr \
+    Accord
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
