8e0fa3122744d340ee33d752319646f59df7c4e7
SebaUbuntu <seba@sebaubuntu.dev>
2026-07-21 06:20:12 +0700

    marble: Import libinit for marble variant
    Co-Authored-By: Ramii Ahmed <ramy@ahmedramy.com>
    Co-Authored-By: luk1337 <priv.luk@gmail.com>
    Co-Authored-By: Arian <arian.kulmer@web.de>
    Co-Authored-By: Mohammed Althaf Thayyil <me@althafvly.com>
    Co-Authored-By: YuKongA <70465933+YuKongA@users.noreply.github.com>
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- lineage_marble.mk
+++ lineage_marble.mk
@@ -13,13 +13,8 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 PRODUCT_NAME := lineage_marble
 PRODUCT_DEVICE := marble
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_BRAND := POCO
-PRODUCT_MODEL := 23049PCD8G
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="marble_global-user 15 AQ3A.241006.001 OS2.0.218.0.VMRMIXM release-keys" \
-    BuildFingerprint=POCO/marble_global/marble:15/AQ3A.241006.001/OS2.0.218.0.VMRMIXM:user/release-keys \
-    DeviceProduct=marble \
-    SystemName=marble_global
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := Redmi/marble/marble:15/AQ3A.250226.002/OS3.0.5.0.VMRCNXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
