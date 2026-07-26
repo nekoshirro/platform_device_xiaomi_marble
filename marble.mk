129c9fa17514b18d77effe7c9662ac31f9788350
Hafidz Muzakky <ais.muzakky@gmail.com>
2026-07-26 11:04:28 +0700

    marble: ASCP bringup
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- marble.mk
+++ marble.mk
@@ -7,10 +7,10 @@
 # Inherit from marble device
 $(call inherit-product, device/xiaomi/marble/device.mk)
 
-# Inherit from common lineage configuration
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit from common ASCP configuration
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_marble
+PRODUCT_NAME := custom_marble
 PRODUCT_DEVICE := marble
 PRODUCT_MANUFACTURER := Xiaomi
 
@@ -18,3 +18,11 @@ PRODUCT_MANUFACTURER := Xiaomi
 BUILD_FINGERPRINT := Redmi/marble/marble:15/AQ3A.250226.002/OS3.0.5.0.VMRCNXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# ASCP Flag
+TARGET_FACE_UNLOCK_SUPPORTED := true
+WITH_GMS := true
+WITH_REVANCED := false
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_ENABLE_BLUR := true
