bd4658b06edf133e14b8a4bfcc1dc2bdf4ddac99
Hafidz Muzakky <ais.muzakky@gmail.com>
2026-08-19 12:02:14 +0700

    marble: Adapt to Shinkai Project
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- shinkai_marble.mk
+++ shinkai_marble.mk
@@ -12,7 +12,7 @@ $(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
 TARGET_SCREEN_WIDTH := 1080
 
-PRODUCT_NAME := custom_marble
+PRODUCT_NAME := shinkai_marble
 PRODUCT_DEVICE := marble
 PRODUCT_MANUFACTURER := Xiaomi
 
@@ -20,3 +20,4 @@ PRODUCT_MANUFACTURER := Xiaomi
 BUILD_FINGERPRINT := Redmi/marble/marble:15/AQ3A.250226.002/OS3.0.5.0.VMRCNXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+SHINKAI_MAINTAINER := Hafidz_Muzakky
